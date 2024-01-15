c0f07e7590504cca918158a79308fe4249c7b185
JakubAdam28 <146720226+JakubAdam28@users.noreply.github.com>
2024-01-15 13:10:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
 
+radio.setTransmitSerialNumber(true)
+radio.setTransmitPower(4) // 0..7
+//radio.setGroup() // 0..255
+
+
+
+
+radio.setGroup(15)
+
+input.onButtonPressed(Button.A, function() {
+    
+    radio.sendNumber(randint(1, 10))
+    
+})
+
+input.onButtonPressed(Button.B, function() {
+    
+    radio.sendString("Nickha")
+})
+input.onButtonPressed(Button.AB, function() {
+    
+    radio.sendValue("name", 0)
+})
